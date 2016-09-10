d7502a0c15e14bad2eaa8ab91a867515d7383035
Daniel Graaf <dg1613@ic.ac.uk>
2016-09-10 20:44:22 +0100

    Added facebook login and encryption for passwords

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '232903053774477',
         'clientSecret'  : '735c50773f2c38c891bd4ec568e86a04',
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL'   : 'https://www.plaats.co.uk/auth/facebook/callback',
     },
 
 };
