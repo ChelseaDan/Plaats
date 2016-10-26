8f90daa1466fcc30b6d0075e97f14b2e2ad6aee5
Daniel Graaf <dg1613@ic.ac.uk>
2016-10-26 14:54:59 +0100

    tidied up css

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -7,6 +7,7 @@ var path = require('path');
 var resemble = require('node-resemble-js');
 
 module.exports.upload = function (req, res, next) {
+
     if (!req.file.mimetype.startsWith('image/')) {
         return res.status(422).json({
             error: 'The uploaded file must be an image'
@@ -23,6 +24,21 @@ module.exports.upload = function (req, res, next) {
 
     var image = new Image({ email: "ABC123", imagePath: req.file.path });
     image.save();
+
+    /*console.log(req);
+    console.log("-------------------");
+    console.log(res);
+    console.log("-------------------");
+    console.log(next);*/
+    fs.open(path.join(__dirname,'./../../uploads/06d5dd43f6ec664992c30b06041434ac1476562359611.jpeg'), 'r', function(err, file) {
+        console.log(file);
+        file.dimensions.width = 200;
+        file.dimensions.height = 300;
+        fs.writefile('a.jpeg', file);
+    });
+    //croppedImage.dimensions.width = 200;
+    //croppedImage.dimensions.height = 300;
+    //conosle.log(croppedImage);
 
     return res.status(200).send(req.file);
     /*fs.readdir(path.join(__dirname, './../../uploads'), function (err, files) {
